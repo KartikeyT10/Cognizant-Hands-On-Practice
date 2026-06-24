af50d50bfa946ad065c506c9b8897185413296a9
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-06-24 23:37:42 +0530

    Update MyServiceTest.java

--- Week 1/TDD using JUnit5 and Mockito/3. Mockito exercises/Exercise 2 - Verifying Interactions/MyServiceTest.java
+++ Week 1/TDD using JUnit5 and Mockito/3. Mockito exercises/Exercise 2 - Verifying Interactions/MyServiceTest.java
@@ -7,16 +7,12 @@ public class MyServiceTest {
     @Test
     public void testVerifyInteraction() {
 
-        // Create mock object
         ExternalApi mockApi = mock(ExternalApi.class);
 
-        // Create service using mock
         MyService service = new MyService(mockApi);
 
-        // Call method
         service.fetchData();
 
-        // Verify interaction
         verify(mockApi).getData();
     }
 }
