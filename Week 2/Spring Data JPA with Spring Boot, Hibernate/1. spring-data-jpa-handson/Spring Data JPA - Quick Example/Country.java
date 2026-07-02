3d429628079f73ae1e55bc525fe373e8269286b2
Kartikey Tiwari <tiwarikartikey03@gmail.com>
2026-07-02 12:58:00 +0530

    added country entity class

---
+++ Week 2/Spring Data JPA with Spring Boot, Hibernate/1. spring-data-jpa-handson/Spring Data JPA - Quick Example/Country.java
@@ -0,0 +1,23 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "co_code")
+    private String code;
+
+    @Column(name = "co_name")
+    private String name;
+
+    // Getters and Setters
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
